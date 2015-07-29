dbc279259800069adcd2636c37ca86cf6c23820a
jamesthe500 <jamesddaniels@gmail.com>
2015-07-29 11:54:55 -0500

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res){
+   res.sendFile(path.join(__dirname, "./public/views/index.html"))
+});
+
+/*app.get('/', function(req, res){
+   res.sendFile(path.join(__dirname, "./public/styles/style.css"))
+});*/
+
+app.listen(app.get('port'), function(){
+   console.log('listening on port: ' + app.get('port'));
+});
